37925ecdb0ce8c1081db7336e1e641e16eb247d5
seheesf88 <seheesf88@gmail.com>
2019-11-20 23:03:59 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import HomeContainer from './HomeContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const My404 = () => {
+  return(
+    <div>
+      ...error...sorry...
     </div>
-  );
+  )
+}
+
+const App = () => {
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/" component={ HomeContainer } />
+        <Route component={ My404 } />
+      </Switch>
+    </main>
+  )
 }
 
 export default App;
---
+++ src/HomeContainer/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class HomeContainer extends Component {
+  constructor(){
+    super()
+  }
+  render(){
+    return (
+      <div>
+        <h1>Frontend setup is done</h1>
+      </div>
+    )
+  }
+}
+export default HomeContainer
